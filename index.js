731d75eeb9213547efa5d487395fcb6c07386a4f
AssiarDev <halibaraissa@gmail.com>
2024-10-24 11:27:28 +0200

    etape 4 améliorer

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // Etape 1 qui se transfome en étape 4
 // Créer une fonction qui demande à l'utilisateur d'entrer un nombre
-const askNumber = entryNumber => {
-    entryNumber = prompt('Joueur 1 : Veuillez entrer un nombre', "")
+const askNumber = (message) => {
+    let entryNumber = prompt(message);
     while(isNaN(entryNumber) || entryNumber < 0 || entryNumber > 50){
-        alert('Nombre invalide')
-        break
+        alert('Nombre invalide, veuillez saisir un nombre ou un nombre entre 0 et 50')
+        entryNumber = prompt(message);
     } 
-    return entryNumber
+    return Number(entryNumber)
 };
 // Etape 2
 // Créer une fonction didWin qui a comme paramètre givenNumber
 const didIWin = (givenNumber, targetNumber) => {
-    targetNumber = 22;
     console.log(givenNumber)
     // condition pour trouver le nombre 
     if (givenNumber < targetNumber){
@@ -26,12 +25,17 @@ const didIWin = (givenNumber, targetNumber) => {
 };
 // Etape 3
 // Créer une fonction gamePlay qui gère le jeu
-let boucle = false
 const gamePlay = () => {
+    // nombre à trouver
+    const targetNumber = askNumber("Joueur 1, entrez un nombre entre 0 et 50 que le joueur 1 doit deviner :");
+    let boucle = false; 
     while (!boucle){
-        userGuess = askNumber();
-        boucle = didIWin(userGuess);
+        const userGuess = askNumber("Joueur 2, entrez votre nombre pour devniner celui du joueur 1 :");
+        boucle = didIWin(userGuess, targetNumber);
+        if(boucle){
+            alert('Félicitation Joueur 2, vous avez gagné !')
+        }
     }
 };
-
+// Lancement du jeu 
 gamePlay();
